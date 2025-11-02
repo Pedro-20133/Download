14cc216a549ffb402f20371fb769f8d9a46907c9
Pedro-20133 <pedroHlq20133@gmail.com>
2025-11-01 23:58:53 -0300

    Fix link removal and add second download link

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ link.download = 'teste.txt';
 document.body.appendChild(link);
 link.click();
 document.body.removeChild(link);
+
+const link = document.createElement('a');
+link.href = 'Download/teste2.txt';
+link.download = 'teste.txt';
+document.body.appendChild(link);
+link.click();
+document.body.removeChild(link);
